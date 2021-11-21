b28197b870c01d9872ae19ad920a2a073e085781
Cervon Wong <tcrammy@gmail.com>
2021-11-21 17:50:38 +0800

    Inform website progress on home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="home">
-    <h1>Welcome to my website</h1>
+    <h1>Hi, welcome to my website :)</h1>
+    <p>This website is still work in progress...</p>
+    <a href="https://play.cervonwong.com"
+      >In the meantime, visit my playground</a
+    >
   </div>
 </template>
 
