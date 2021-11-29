82ec480c4cd4e9ec709d6b67132e32159248d82b
Cervon Wong <tcrammy@gmail.com>
2021-11-29 21:49:11 +0800

    Increase margin between navigation bar and CTA at larger screen sizes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -78,6 +78,14 @@ section {
 
 .section-cta {
   margin-top: 2.5rem; /* 40px */
+
+  @include bp-tablet-and-up {
+    margin-top: 3.75rem; /* 60px */
+  }
+
+  @include bp-laptop-and-up {
+    margin-top: 5rem; /* 80px */
+  }
 }
 
 .cta-link:hover {
