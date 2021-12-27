26ce9f3b56078e9a7471348cff3be4fd36756c3f
Cervon Wong <tcrammy@gmail.com>
2021-12-27 21:39:15 +0800

    Update copy on "About me" section of homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -25,9 +25,9 @@
           was to code and design things.
         </p>
         <p>
-          Fast forward to today, and I'm still seeking new knowledge in
-          front-end and design. I'm always on the lookout for technologies to
-          learn! (Currently learning Vue.js).
+          These days, I'm still seeking new knowledge in front-end and design.
+          I'm always on the lookout for technologies to learn! (Currently
+          learning Vue.js).
         </p>
         <router-link :to="{ name: 'About me' }" class="about-me-link">
           Learn more about me
